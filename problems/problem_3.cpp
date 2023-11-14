609dbe6f04c11ee71260edbdd44711b40d47bd3f
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2023-11-14 22:24:36 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -5,15 +5,15 @@ std::string problemSolution3(float height, char S) {
 
     if('M' == S && height < 1.70){
         
-    } else if(S == 'M' && (height >= 1.70 && height < 1.85)){
-        
     } else if(S == 'M' && height >= 1.85){
         
-    } else if(S=='F' && height < 1.60){
+    } else if(S == 'M' && (height >= 1.70 && height < 1.85)){
         
     } else if(S == 'F' && (height >= 1.60 && height < 1.75)){
         
-    } else if(S == 'F' && height >= 1.75){
+    }  else if(S=='F' && height < 1.60){
+        
+    }  else if(S == 'F' && height >= 1.75){
     }
     return 0;
     // use return to return your result
