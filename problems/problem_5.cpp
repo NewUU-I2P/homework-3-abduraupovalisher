6848842edfcd52a6f22667d4d3a4391cf8ea93d8
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2023-11-14 21:06:46 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,20 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
-   return result;
+    if(operation == '+'){
+       
+        result = x + y;
+    } else if(operation == '-'){
+       
+        result = x - y;
+    } else if(operation == '*'){
+       
+        result = x * y;
+    } else if((operation == '/') && (y != 0)){
+       
+        result = x / y;
+    } else if( y == 0){
+       
+    }
+    return result;
 }
